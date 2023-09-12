c9aa9017681138c6d072ec35807d149fa16a04b6
SeM2x <houssem135.302@gmail.com>
2023-09-12 11:43:14 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a given number
+ * @n: the given number
+ *
+ * Return: 1, -1, 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
