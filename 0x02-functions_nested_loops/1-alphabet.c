d7b46a35adc0fb70f1031bcff023e4453473b0b1
SeM2x <houssem135.302@gmail.com>
2023-09-12 10:51:29 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints lowecase alphabet
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
