25e8afb368562225b98a3b7043ed4eb01823f712
SeM2x <houssem135.302@gmail.com>
2023-09-12 12:07:30 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,11 +13,8 @@ int print_last_digit(int n)
 
 	last_digit = n % 10;
 	if (last_digit < 0)
-	{
-		_putchar('-');
 		_putchar(-1 * last_digit + '0');
-	} else
-		_putchar(last_digit + '0');
-
+		return (-1 * last_digit);
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
