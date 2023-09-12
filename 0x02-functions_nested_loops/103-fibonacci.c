a79cf62ad8d458cd737e3917197551fe911e58a1
SeM2x <houssem135.302@gmail.com>
2023-09-12 20:20:45 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even-valued terms in the fabonacci sequence
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i, j, x, count;
+
+	i = 1;
+	j = 2;
+	count = 0;
+	while (i < 4000000)
+	{
+		if (i % 2 == 0)
+			count += i;
+		x = i;
+		i = j;
+		j += x;
+	}
+	printf ("%d\n", count);
+	return (0);
+}
