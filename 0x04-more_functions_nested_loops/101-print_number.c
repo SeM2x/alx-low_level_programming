cf16dfc743ef6cdd613c1a3bc9daa2c30cb79940
SeM2x <houssem135.302@gmail.com>
2023-09-15 00:18:11 +0100

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,9 +12,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -1 * n;
+		n = -n;
 	}
-	if (n / 10)
+	if (n > 0)
 		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }
