a2426eff5b03bee8a51bfec05753c7e94b7f4be9
SeM2x <houssem135.302@gmail.com>
2023-09-14 17:16:58 +0100

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,59 +1,21 @@
 #include "main.h"
 
 /**
- *_pow - calculates 10 to the power of a given number
- *@l: the given number
- *
- *Return: 10 ^ l
+ *print_number - prints an integer using _putchar
+ *@n: the integer to be printed
  */
-
-int _pow(int l)
-{
-	int i, res;
-
-	res = 1;
-	for (i = 0; i < l; i++)
-		res = res * 10;
-
-	return (res);
-}
-
-/**
- *print_number - prints a number
- *@n: the given number
- *
- *Return: void
- */
-
 void print_number(int n)
 {
-	int length, m, i;
-
-	length = 0;
-
-	if (n == 0)
+	if (n < 0)
 	{
-		_putchar('0');
+		_putchar('-');
+		n = -n;
 	}
-	else
+
+	if (n / 10 != 0)
 	{
-		if (n < 0)
-		{
-			n = -1 * n;
-			_putchar('-');
-		}
-
-		m = n;
-
-		while (m > 0)
-		{
-			m = m / 10;
-			length++;
-		}
-
-		for (i = length - 1; i >= 0; i--)
-		{
-			_putchar((n / _pow(i)) % 10 + '0');
-		}
+		print_number(n / 10);
 	}
+
+	_putchar(n % 10 + '0');
 }
