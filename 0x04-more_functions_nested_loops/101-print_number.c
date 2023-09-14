738be584d3064e06a30fdacb11fe28f8a773ca0e
SeM2x <houssem135.302@gmail.com>
2023-09-15 00:21:27 +0100

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,7 +14,17 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+
+	int divisor = 1;
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
 }
