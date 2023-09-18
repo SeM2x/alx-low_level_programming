9f3124e1914543a384e7b45a54d4b248241d627f
Houssem Eddine <87502570+SeM2x@users.noreply.github.com>
2023-09-19 00:03:00 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,33 +3,6 @@
 #include <stdlib.h>
 
 /**
- *getRandomChar - generates random chars
- *
- *Return: char.
- */
-
-char getRandomChar(void)
-{
-	int lower, upper, digit, random;
-
-	lower = 'a' + rand() % 26;
-	upper = 'A' + rand() % 26;
-	digit = '0' + rand() % 10;
-	random = rand() % 3;
-	switch (random)
-	{
-		case 0:
-			return (lower);
-		case 1:
-			return (upper);
-		case 2:
-			return (digit);
-		default:
-			return (-1);
-	}
-}
-
-/**
  *main - generates random keys
  *
  *Return: 0.
@@ -37,31 +10,6 @@ char getRandomChar(void)
 
 int main(void)
 {
-	int i, currSum, remSum;
-	char pwd[50];
-
-	srand(time(NULL));
-	currSum = 0;
-	i = 0;
-	while (currSum < 2772)
-	{
-		remSum = 2772 - currSum;
-		if (remSum < 126)
-		{
-			pwd[i] = remSum;
-			currSum += remSum;
-		}
-		else
-		{
-			pwd[i] = getRandomChar();
-			currSum += pwd[i];
-		}
-
-		i++;
-	}
-
-	pwd[i] = '\0';
-	printf("%s", pwd);
-
+	printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
 	return (0);
 }
