69f6f53eed83ba3eb8f1237f45f4e2512803926e
SeM2x <houssem135.302@gmail.com>
2023-09-18 11:35:00 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to print
+ *
+ * Return: 0
+*/
+
+
+
+void print_rev(char *s)
+{
+	int i;
+}
