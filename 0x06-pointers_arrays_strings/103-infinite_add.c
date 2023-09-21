c5961acbc9fb0a956d9bd4774f8c43fe518dd469
SeM2x <houssem135.302@gmail.com>
2023-09-21 17:35:20 +0100

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,28 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ *reverse_string -  add two integer
+ *@str: string
+ *
+ *Return: void.
+ */
+
+void reverse_string(char *str)
+{
+	int start = 0;
+	int end = strlen(str) - 1;
+
+	while (start < end)
+	{
+		char temp = str[start];
+
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+}
 
 /**
  *infinite_add -  add two integer
@@ -7,31 +31,32 @@
  *@r: buffer
  *@size_r: size of buffer
  *
- *Return: void.
+ *Return: string.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
+	int len1, len2, carry, maxLen, i, j, k, num1, num2, sum;
 
-	int carry = 0;
-	int maxLen = len1 > len2 ? len1 : len2;
+	len1 = strlen(n1);
+	len2 = strlen(n2);
+
+	carry = 0;
+	maxLen = len1 > len2 ? len1 : len2;
 
 	if (size_r <= maxLen + 1)
 		return (0);
 
-	int i = len1 - 1;
-	int j = len2 - 1;
-	int k = 0;
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
 
 	while (i >= 0 || j >= 0 || carry > 0)
 	{
-		int num1 = (i >= 0) ? n1[i] - '0' : 0;
-		int num2 = (j >= 0) ? n2[j] - '0' : 0;
+		num1 = (i >= 0) ? n1[i] - '0' : 0;
+		num2 = (j >= 0) ? n2[j] - '0' : 0;
 
-		int sum = num1 + num2 + carry;
-
+		sum = num1 + num2 + carry;
 		carry = sum / 10;
 		r[k] = (sum % 10) + '0';
 		k++;
@@ -42,13 +67,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	r[k] = '\0';
 
-	for (int start = 0, end = k - 1; start < end; start++, end--)
-	{
-		char temp = r[start];
-
-		r[start] = r[end];
-		r[end] = temp;
-	}
+	reverse_string(r);
 
 	return (r);
 }
