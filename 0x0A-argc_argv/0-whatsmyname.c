db61e4e3edd0166492808db272d280c9595cace9
SeM2x <houssem135.302@gmail.com>
2023-10-02 16:24:49 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - prints file name
+ * @argc: args count
+ * @argv: args array
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
