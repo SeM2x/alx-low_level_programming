3e9d208bedfa4848386ed1ece9cab31fb2aeabbb
SeM2x <houssem135.302@gmail.com>
2023-10-02 16:29:53 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints file name
+ * main - prints args number
  * @argc: args count
  * @argv: args array
  *
@@ -10,6 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	argv[0] = argv[0];
+	printf("%d\n", argc);
 	return (0);
 }
