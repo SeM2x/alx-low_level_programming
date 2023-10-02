cc11ea06a303b52698b377843fa6cd464cbf2600
SeM2x <houssem135.302@gmail.com>
2023-10-02 16:36:53 +0100

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints args number
+ * @argc: args count
+ * @argv: args array
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
