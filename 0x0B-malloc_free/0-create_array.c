3e789ba1c1f0bc83ea1e8d75d9a17fda5a9eb52a
SeM2x <houssem135.302@gmail.com>
2023-10-03 13:02:31 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,10 +15,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
-	if (size < 1)
-		return (NULL);
-
 	arr = malloc(sizeof(char) * size);
+	if (size < 1 || !arr)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
