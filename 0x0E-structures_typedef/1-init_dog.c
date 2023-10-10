e0aaa365cc41e95a212e1ab0f99ffc5103bafe2a
SeM2x <houssem135.302@gmail.com>
2023-10-10 18:32:18 +0100

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = strdup(name);
-	d->age = age;
-	d->owner = strdup(owner);
+	if (d != NULL)
+	{
+		d->name = strdup(name);
+		d->age = age;
+		d->owner = strdup(owner);
+	}
 }
