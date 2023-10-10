cba90145fdd2e22fc43a997409e57787d6f7b31c
SeM2x <houssem135.302@gmail.com>
2023-10-10 18:07:09 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: dog properties
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
