7818d79d63366912dcfd1178543459edae007b5e
SeM2x <houssem135.302@gmail.com>
2023-10-11 11:34:28 +0100

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && action != NULL)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < (int)size; i++)
 		{
 			action(array[i]);
 		}
