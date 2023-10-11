c63886296583668f1b4510c78501a0ed8d307035
SeM2x <houssem135.302@gmail.com>
2023-10-11 11:42:05 +0100

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i])
+		if (cmp(array[i]))
 			return (i);
 	}
 
-	return (-1)
+	return (-1);
 
 
 }
