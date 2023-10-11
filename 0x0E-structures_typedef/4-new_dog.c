f1a618189432580768ee6150230bbb1a55e256c8
SeM2x <houssem135.302@gmail.com>
2023-10-11 01:09:18 +0100

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: name
+ * @age: age
+ * @owner: ownwe
+ *
+ * Return: a pointer to the new allocated memory or NULL;
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = strdup(name);
+	d->age = age;
+	d->owner = strdup(owner);
+
+	return (d);
+}
