868406561985a7d0e68278dc3c52187cf264f3a9
SeM2x <houssem135.302@gmail.com>
2023-10-19 14:32:04 +0100

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 void startup_function(void) __attribute__((constructor));
 void startup_function(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-		I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
