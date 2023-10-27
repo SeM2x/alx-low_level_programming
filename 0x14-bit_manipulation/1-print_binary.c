30b1c05e9c693944d7a46c27d5a5d0bc12ab702d
SeM2x <houssem135.302@gmail.com>
2023-10-27 02:09:35 +0100

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *print_binary - prints the binary representation of a number.
+ *@n: number
+ *
+ *Return: Void.
+ */
+void print_binary(unsigned long int n)
+{
+	int bit_count = sizeof(unsigned long int) * 8;
+	unsigned int mask;
+	int i, print;
+
+	print = 0;
+
+	if (n == 0)
+		_putchar('0');
+
+	for (i = bit_count - 1; i >= 0; i--)
+	{
+		mask = 1UL << i;
+		if ((n & mask))
+			print = 1;
+		if (print)
+		{
+			if ((n & mask) == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar('1');
+			}
+		}
+	}
+
+	_putchar('\n');
+}
