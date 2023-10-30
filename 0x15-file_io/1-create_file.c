22dcb84f70449b046973404d011fa97818bf6ec8
SeM2x <houssem135.302@gmail.com>
2023-10-30 16:12:54 +0100

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	write(fd, text_content, strlen(text_content));
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
