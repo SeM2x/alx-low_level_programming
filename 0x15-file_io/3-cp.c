cdff6bcc73888ec171e5ff4173d9e307dda9a1b9
Houssem Eddine <87502570+SeM2x@users.noreply.github.com>
2023-10-31 11:08:29 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,148 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-#include <string.h>
+#include "main.h"
+#define STDERR_FILENO 2
 
-void print_error(const char *message)
+/**
+ * cpy_data - copies a file content to another.
+ * @ffd: file from definer
+ * @ftd: file to definer
+ * @b: buffer
+ * av: argv
+ *
+ * Return - Void.
+*/
+void cpy_data(char **av, int ffd, int ftd, char *b)
 {
-	fprintf(stderr, "%s\n", message);
-	exit(98);
+    int sz;
+
+	sz = 0;
+	do {
+		sz = read(ffd, b, 1024);
+		if (sz < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+
+		if (sz > 0 && write(ftd, b, strlen(b)) < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (sz > 0);
 }
-
-void print_elf_header_info(Elf64_Ehdr *header)
-{
-	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
+/**
+ * main -  copies the content of a file to another file.
+ * @ac: argc
+ * @av: argv
+ * Return: 0.
+*/
+int main(int ac, char **av)
+	@@ -15,7 +46,6 @@ int main(int ac, char **av)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
-	printf("\n");
-
-	printf("  Class:                             ");
-	switch (header->e_ident[EI_CLASS])
+	ffd = open(av[1], O_RDONLY);
+	if (ffd < 0)
 	{
-		case ELFCLASS32:
-			printf("ELF32\n");
-			break;
-		case ELFCLASS64:
-			printf("ELF64\n");
-			break;
-		default:
-			printf("<unknown>\n");
-			break;
-	}
-
-	printf("  Data:                              ");
-	switch (header->e_ident[EI_DATA])
+	@@ -26,23 +56,18 @@ int main(int ac, char **av)
+	if (ftd < 0)
 	{
-		case ELFDATA2LSB:
-			printf("2's complement, little-endian\n");
-			break;
-		case ELFDATA2MSB:
-			printf("2's complement, big-endian\n");
-			break;
-		default:
-			printf("<unknown>\n");
-			break;
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 	}
-
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-
-	printf("  OS/ABI:                            ");
-	switch (header->e_ident[EI_OSABI])
+	cpy_data(av, ffd, ftd, b);
+	if (close(ffd) < 0)
 	{
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_HPUX:
-			printf("HP-UX\n");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("NetBSD\n");
-			break;
-		case ELFOSABI_LINUX:
-			printf("Linux\n");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("Sun Solaris\n");
-			break;
-		case ELFOSABI_AIX:
-			printf("AIX\n");
-			break;
-		case ELFOSABI_IRIX:
-			printf("IRIX\n");
-			break;
-		case ELFOSABI_FREEBSD:
-			printf("FreeBSD\n");
-			break;
-		case ELFOSABI_TRU64:
-			printf("TRU64 UNIX\n");
-			break;
-		case ELFOSABI_MODESTO:
-			printf("Novell Modesto\n");
-			break;
-		case ELFOSABI_OPENBSD:
-			printf("OpenBSD\n");
-			break;
-		default:
-			printf("<unknown>\n");
-			break;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffd);
+		exit(100);
 	}
-
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-
-	printf("  Type:                              ");
-	switch (header->e_type)
+	if (close(ftd) < 0)
 	{
-		case ET_NONE:
-			printf("NONE (None)\n");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)\n");
-			break;
-		case ET_EXEC:
-			printf("EXEC (Executable file)\n");
-			break;
-		case ET_DYN:
-			printf("DYN (Shared object file)\n");
-			break;
-		case ET_CORE:
-			printf("CORE (Core file)\n");
-			break;
-		default:
-			printf("<unknown>\n");
-			break;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ftd);
+		exit(100);
 	}
-
-	printf("  Entry point address:               0x%lx\n", (unsigned long) header->e_entry);
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		print_error("Usage: elf_header elf_filename");
-	}
-
-	const char *filename = argv[1];
-	int fd;
-	Elf64_Ehdr header;
-
-	if ((fd = open(filename, O_RDONLY)) == -1 || read(fd, &header, sizeof(header)) == -1)
-	{
-		print_error("Error: Unable to read ELF header");
-	}
-
-	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
-	{
-		print_error("Error: Not an ELF file");
-	}
-
-	print_elf_header_info(&header);
-
-	close(fd);
-	return 0;
+	return (0);
 }
