382d72715fb11b506185d4e07d0537833b33cce5
SeM2x <houssem135.302@gmail.com>
2024-01-23 22:35:44 +0100

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -20,7 +20,7 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while(node)
 		{
-			printf("'%s': '%s'", node->key, node->value);
+			printf("'%s': '%s', ", node->key, node->value);
 			node = node->next;
 		}
 	}
