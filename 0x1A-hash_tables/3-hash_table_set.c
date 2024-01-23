2fbf3d6db9cfc09274312c7493b90f6767ef5db0
SeM2x <houssem135.302@gmail.com>
2024-01-23 21:54:24 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,14 +11,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node, *tmp;
-	unsigned int size;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || ht->size == 0 || key == NULL)
 		return (0);
 
-	size = sizeof(ht) / sizeof(hash_node_t *);
-	index = key_index((unsigned char) key, size);
+	index = key_index((unsigned char *) key, ht->size);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
@@ -27,14 +25,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 	node->next = NULL;
 
-	if (ht[index])
+	if (ht->array[index])
 	{
-		tmp = ht[index];
-		ht[index] = node;
+		tmp = ht->array[index];
+		ht->array[index] = node;
 		node->next = tmp;
 	}
 	else
-		ht[index] = node;
+		ht->array[index] = node;
 
 	return (1);
 }
