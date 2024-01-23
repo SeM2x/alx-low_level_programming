1ce6406785db634bc61617caab6b356fdbe83dcf
SeM2x <houssem135.302@gmail.com>
2024-01-23 22:22:43 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node = NULL;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
 
