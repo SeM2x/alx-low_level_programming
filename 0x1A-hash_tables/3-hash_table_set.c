6efda7899edde78b0ebebe53e0ea5c7656113b74
SeM2x <houssem135.302@gmail.com>
2024-01-23 21:48:33 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	size = sizeof(ht) / sizeof(hash_node_t *);
-	index = key_index(key, size);
+	index = key_index((unsigned char) key, size);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
