67db1c16ee1a66b08318e3b4b2733f7de3e56f2e
SeM2x <houssem135.302@gmail.com>
2024-02-04 16:03:18 +0100

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -24,7 +24,7 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			for (i = 0; i < ht->size; i++)
 			{
-				if (hash_table->array[i])
+				if (ht->array[i])
 					free_list(ht->array[i]);
 			}
 			free(ht->array);
