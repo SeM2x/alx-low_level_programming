3db675898c29e39c2c5a105da4cf53f89df4b5fa
SeM2x <houssem135.302@gmail.com>
2024-02-04 16:21:06 +0100

    6-hash_table_delete.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		if (ht->array[index])
+		{
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
 			free(ht->array[index]);
+			node->next = ht->array[index]->next;
+		}
 		ht->array[index] = node;
 	}
 
