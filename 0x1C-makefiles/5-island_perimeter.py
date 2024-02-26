16b353ea25f0d7070b95c04452b0d80a39389978
SeM2x <houssem135.302@gmail.com>
2024-02-26 08:38:15 +0100

    .\.gitignore

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,12 +13,13 @@ def island_perimeter(grid):
         int: The perimeter of the island.
     """
     perimeter = 0
-    row = []
-    for i in range(len(grid[0])):
-         row += [0]
-    grid += row
+    if 1 in grid[-1]:
+        grid = grid + [len(grid[0]) * [0]]
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            if grid[i][-1] == 1:
+                grid[i] = grid[i] + [0]
             if grid[i][j] == 1:
                 if grid[i-1][j] == 0:
                     perimeter += 1
@@ -28,4 +29,5 @@ def island_perimeter(grid):
                     perimeter += 1
                 if grid[i+1][j] == 0:
                     perimeter += 1
+
     return perimeter
