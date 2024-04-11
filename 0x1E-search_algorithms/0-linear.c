495a5c3f2358d8f6da8f823595825fd3305c6bc1
SeM2x <houssem135.302@gmail.com>
2024-04-11 12:38:43 +0100

    .\.gitignore

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i = 0;
 
-    if (!array)
+    if (!array || !size)
         return (-1);
     
     for (; i < size; i++)
     {
         printf("Value checked array[%ld] = [%d]\n", i, array[i]);
         if (array[i] == value)
-            return (value);
+            return (i);
     }
 
     return (-1); 
